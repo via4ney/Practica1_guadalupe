a8a7da8d8e2e0f35e3adda3dbaa2642f8efc331c
via4ney <89814902+via4ney@users.noreply.github.com>
2021-09-07 18:04:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+index.Js
+function get IMC(){
+  var peso:parse Float(document.get Element By id
+    (´peso´).valve);
+  
+  var peso:parse Float(document.get Element By id
+    (´peso´).valve);
+   var altura:parse Float(document.get Element By id
+    (´altura´).valve);
+  var imc=peso/(altura . altura);
+  document.get Element By id("imc").inner HTML=imc
+}
+  
+  
 
